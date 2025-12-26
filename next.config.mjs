b0c6af3f937a9a194d67966fb19548c4519b1223
Qin Liu <qinliu2020@gmail.com>
2025-12-25 22:18:48 -0800

    Update the config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export', // <--- Add this line
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for GitHub Pages
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
